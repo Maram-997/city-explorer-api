dd992ef9fd9cde406eba4ec0d89c6266ee60669d
Maram Abu Murad <maramabumurad97@gmail.com>
2021-07-07 16:43:53 +0300

    finishing back-end

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const weatherData = require('./weather.json')
+const axios = require('axios');
 const server = express();
 const PORT = process.env.PORT;
 server.use(cors());
 
 server.get('/weather', getWeatherHandler)
-server.get('/weather', getMoviesHandler)
+server.get('/movie', getMoviesHandler)
 // http://localhost:3001/weather?cityName=Amman
 // server.get('/weather',(req,res)=>{
 //     let selectedCity = weatherData.find(element =>{
@@ -29,31 +30,38 @@ server.get('/weather', getMoviesHandler)
 
 // Movies URL
  // https://api.themoviedb.org/3/search/movie?api_key=baba718b7f864fb52e546cdd76bb4f65&query=Action
-
+// http://localhost:3001/movie?cityName=Amman
 function getMoviesHandler(req,res){
-    let sQuery = req.query.searchQuery;
+    let sQuery = req.query.cityName;
     let moviesUrl =`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${sQuery}`
  axios
  .get(moviesUrl)
- .then(moviesData =>{ moviesUrl.data.results.map(element => {
-     return new Movie (element.title , element.overview, element.average_votes, element.total_votes. element.image_url, element.popularity, element.released_onZ)
+ .then(moviesData =>{
+     let selectedMovie = moviesData.data.results.map(movie => {
+         return new Movie (movie)
+     })
+     res.send(selectedMovie)
  }) 
-     res.send(moviesData)
- })
+   
+  
 }
+
 
 // https://api.weatherbit.io/v2.0/forecast/daily?city=london&key=3e950fccc65f477c92a03fccf5b5db9b&days=5
 
+
+// http://localhost:3001/weather?cityName=Amman
 function getWeatherHandler (req, res) {
-    let sQuery = req.query.searchQuery;
+    let sQuery = req.query.cityName;
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${sQuery}&key=${process.env.WEATHER_KEY}&days=5`
     
     axios
     .get(url)
-    .then(weatherData=>{ url.data.map(elemet => {
-        return new Forecast (elemet.valid_date, elemet.weather.description)
-    })
-        res.send(weatherData) 
+    .then(weatherData=>{
+        let selectedCity = weatherData.data.data.map(day => {
+            return new Forecast (day.valid_date, day.weather.description)
+        })  
+        res.send(selectedCity) 
     })
     .catch(error=>{
         res.status(500).send(error)
@@ -77,14 +85,14 @@ server.get('*',(req,res) =>{
  }
  
   class Movie {
-      constructor(title, overview, average_votes, total_votes, image_url, popularity, released_on){
-          this.title = title;
-          this.overview = overview,
-          this.average_votes = average_votes;
-          this.total_votes = total_votes;
-          this.image_url = `https://image.tmdb.org/t/p/w500${this.poster_path}`;
-          this.popularity = popularity;
-          this.released_on = release_date;
+      constructor(movie){
+          this.title = movie.title;
+          this.overview = movie.overview,
+          this.vote_average = movie.vote_average;
+          this.vote_count = movie.vote_count;
+          this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+          this.popularity = movie.popularity;
+          this.released_on = movie.release_date;
           
       }
   }
